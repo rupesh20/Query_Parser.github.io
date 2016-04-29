eb6bf2217d6ea119a58ccf2ed760ff36980b4733
rupesh20 <rupesh.patil741@gmail.com>
2016-04-29 13:26:35 +0530

    Update youtube_parser.py

--- query_parser/bin/youtube_parser.py
+++ query_parser/bin/youtube_parser.py
@@ -1,19 +1,47 @@
-import gdata.youtube
-import gdata.youtube.service
+#!/usr/bin/python
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+import sys
+import json
 
-#youtube api key=AIzaSyDhIco9O-WQkKanqV-9N3gf1c-7EfkWcdA
 
-#hard code for query search
+reload(sys) # system unicode and encoding checking k liye hai 
+
+sys.setdefaultencoding('utf8') # unicode encode codec error ko durr krdiya hai  isne
+
+api_key='AIzaSyDhIco9O-WQkKanqV-9N3gf1c-7EfkWcdA'# meta data ko access krne k liye
+
+# function to search on the list of data of videos and print the results of the query passed as a arguments
+def youtube_search(var, n):
+
+	service= build('youtube','v3',developerKey=api_key)# object service of class build 
+
+	search_response = service.search().list(
+			q=var,#options m s query nikal li gai hai	
+			part="id,snippet", #part mtlb kya milega hame,id/snippet
+			maxResults=n #no of results of search  
+		).execute()
 	
-def SearchQuery(searc_q):
-
-	obj_youtube=gdata.youtube.service.YouTubeService()
-	obj_youtube.developer_key='-XXXXXX-'
-	query=gdata.youtube.service.YouTubeVideoQuery()
-	query.vq=searc_q
+	videos= [ ] #list of videos 
+	#print sys.getdefaultencoding()
 	
-	feed=obj_youtube.YouTubeQuery(query)
-	return PrintVideoFeed(feed)
+	#json data dumping i the text file for thinking purpose
+	with open('1.txt','w') as outfile:
+		json.dump(search_response,outfile)
 
+	for search_res in search_response.get("items",[]):
+		if search_res["id"]["kind"]== "youtube#video":
+			videos.append(" %s "%(search_res["id"]["videoId"]))
 
-SearchQuery('babay')
+	return videos		
+
+"""if __name__ == '__main__':
+	argparser.add_argument("--q",help="Search term", default="Google")
+	argparser.add_argument("--max-results",help="Max results",default=25)
+	args=argparser.parse_args()
+
+	try:
+		youtube_search(args)
+	except HttpError, e:
+		print "%d err:\n %s"%(e.resp.status,e.content)"""
